2be76ae89ab3da50164108fedbf4a2083771cab1
feifei <feifei@feifeis-MacBook-Pro.local>
2017-02-05 21:41:51 -0500

    10.12.3 updates

---
+++ 10.12.3/README.md
@@ -0,0 +1,36 @@
+# X1Yoga-Hackintosh
+
+# SPEC:
+  Thinkpad X1 Yoga
+
+	CPU: I7-6600U
+	IGPU: HD520
+	Display: 1920 x 1080
+	WIFI/BT: Bcm94352z(Lenovo FRU)
+    SSD: SM961
+	BIOS:
+	OS X: 10.12.3
+	BootLoader：Clover+GPT
+
+# Installation/Post-Installation:
+  you can refer to README in 10.11.X folder
+  or taking the advantage in 10.12.X folder
+
+* if you are using NVMe SSD as the boot disk you may encounter 'Couldn't Unmount Disk” Error in Disk Utility' at the end of installation,you always can install in a HDD and CCC it after
+
+* ./kexts/HackrNVMeFamily-10_12_3.kext and./Clover/ACPI/patched/SSDT-NVMe.aml are for NVMe SSD
+* ./kexts/AppleALC.kext : I put it in S/L/E, don't know why is not working in Clover/kexts/Other
+
+# current Problem:
+
+    BT sometime shows not available after wakeup
+    no HDMI codec
+
+
+# Credits and Thanks
+    Scripts are based on RehabMan's Repo with some modifications
+    BIG THANKS FOR 'Great people share their wisdom without asking for anything in return…':
+      RehabMan
+      Pike R. Alpha
+      tluck
+      shmilee
---
+++ 10.12.3/VoodooPS2Controller.py
@@ -0,0 +1,40 @@
+import plistlib as pl
+'''
+this script changes thinkpad TrackPoint(小红点)'s speed as well as adding 3 finger gestures
+'''
+VoodooPS2Trackpad='kexts/VoodooPS2Controller.kext/Contents/PlugIns/VoodooPS2Trackpad.kext/Contents/Info.plist'
+VoodooPS2Keyboard='kexts/VoodooPS2Controller.kext/Contents/PlugIns/VoodooPS2Keyboard.kext/Contents/Info.plist'
+
+TrackpadRedTrackPointpath='IOKitPersonalities,Synaptics TouchPad,Platform Profile,Default'
+_3fingerGesturePath='IOKitPersonalities,ApplePS2Keyboard,Platform Profile,Default'
+
+TrackPoint={
+'MouseMultiplierX':2,
+'MouseMultiplierY':2,
+'MouseScrollMultiplierX':10,
+'MouseScrollMultiplierY':10
+}
+_3fingerGesture={
+'ActionSwipeDown':'3e d, 7d d, 7d u, 3e u',
+'ActionSwipeLeft':'3e d, 7c d, 7c u, 3e u',
+'ActionSwipeRight':'3e d, 7b d, 7b u, 3e u',
+'ActionSwipeUp':'3e d, 7e d, 7e u, 3e u'
+}
+
+# http://stackoverflow.com/questions/11700798/python-accessing-values-nested-within-dictionaries
+def lookup(dic, key, *keys):
+    if keys:
+        return lookup(dic.get(key, {}), *keys)
+    return dic.get(key)
+
+def change(file,path,data):
+    plist=pl.readPlist(file)
+    temp=lookup(plist,*path.split(','))
+    print path
+    for key, value in data.iteritems():
+        print key,value
+        temp[key]=value
+    pl.writePlist(plist,file)
+
+change(VoodooPS2Trackpad,TrackpadRedTrackPointpath,TrackPoint)
+change(VoodooPS2Keyboard,_3fingerGesturePath,_3fingerGesture)
